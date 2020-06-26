a31bc4afab2610c631cdd987e7e7ceb1f5323962
git-pupil <690736803@qq.com>
2020-06-26 20:58:17 +0800

    userController的url修改

--- src/main/java/admin/demo/Controller/UserController.java
+++ src/main/java/admin/demo/Controller/UserController.java
@@ -60,7 +60,7 @@ public class UserController {
     }
 
     @ApiOperation(value = "用户退房")
-    @PostMapping(value = "/register", produces = "application/json")
+    @PostMapping(value = "/checkout", produces = "application/json")
     public Result<Object> checkout(Integer userId, String password){
         User user = userRepository.findUserByUserId(userId);
         if (user == null || !user.password.equals(password)) return Result.error("用户名或密码错误");
