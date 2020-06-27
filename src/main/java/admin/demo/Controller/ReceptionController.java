b147219ea841c5360b9b46a8e960df1fd09ef840
Sprinter1999 <215437588@qq.com>
2020-06-27 10:08:56 +0800

    Modify ReceptionController.java

--- src/main/java/admin/demo/Controller/ReceptionController.java
+++ src/main/java/admin/demo/Controller/ReceptionController.java
@@ -8,12 +8,9 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import admin.demo.Dto.ReceptionBill;
-
+import org.thymeleaf.context.Context;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,20 +22,22 @@ public class ReceptionController {
     @Autowired
     RecordRepository recordrepository;
 
+    @ApiOperation(value = "返回首页")
+    @RequestMapping(value="/",method=RequestMethod.GET)
+    public String getIndex() {return "index";}
+
 
     @ApiOperation(value = "返回账单页面")
     @RequestMapping(value = "/bill",method = RequestMethod.GET)
     public String getBill(){
-        return "bill";
+        return "billPage";
     }
 
     @ApiOperation(value = "根据用户ID查询账单",notes = "返回用户所住房间这段时间的账单")
     @RequestMapping(value = "/bill",method = RequestMethod.POST)
     @ResponseBody
     public ReceptionBill postBill( @RequestParam Integer user_id){
-
         ReceptionBill userbill=new ReceptionBill();
-
          Bill bill=billrepository.getBillById(user_id);
          userbill.setUserId(bill.userId);
          userbill.setRoomId(bill.roomId);
@@ -50,20 +49,19 @@ public class ReceptionController {
     }
 
     @ApiOperation(value = "返回详单页面")
-    @RequestMapping(value = "/invoice",method = RequestMethod.GET)
-    public String getInvoice(){
-        return "invoice";
+    @RequestMapping(value = "/record",method = RequestMethod.GET)
+    public String getRecord(){
+        return "record";
     }
 
     @ApiOperation(value = "根据用户ID查询详单",notes = "返回用户住房时间内的详单")
-    @RequestMapping(value ="/invoice",method =RequestMethod.POST)
+    @RequestMapping(value ="/record",method =RequestMethod.POST)
     @ResponseBody
-    public String postInvoice(Model model,@RequestParam Integer user_id){
-
+    public String postRecord(Model model,@RequestParam Integer user_id){
        //record sort=recordrepository.getByUserId(user_id);
        List<Record> records=new ArrayList<Record>(recordrepository.getByUserId(user_id));
-       model.addAttribute("record",records);
-       return "invoice";
+       model.addAttribute("records",records);
+       return "record";
 
     }
 }
